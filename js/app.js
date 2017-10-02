b8bde31316f6b410dceacee99feec3eb318be6f7
Spencer Harston <spencer314@gmail.com>
2017-10-02 16:51:13 -0600

    Get location in own function
    Changed location to own function to allow for other uses. Added sunrise/sunset info. Changed font. Need to fix sunrise/sunset to non-UTC.

--- js/app.js
+++ js/app.js
@@ -1,29 +1,45 @@
-﻿function weather() {
+﻿function getLocation() {
     var location = document.getElementById("location");
-    var apiKey = '9add6004cd6af626a252478801c67b84';
-    var url = 'https://api.forecast.io/forecast/';
-
-    navigator.geolocation.getCurrentPosition(success, error);   //use geolocation api to get user's current location
     location.innerHTML = "Locating...";
 
+    navigator.geolocation.getCurrentPosition(success, error);   //use geolocation api to get user's current location
+    
+    
     function success(position) {
-        latitude = position.coords.latitude;
-        longitude = position.coords.longitude;
-
+        //debugger;
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
         location.innerHTML = 'Latitude is ' + latitude + '° Longitude is ' + longitude + '°';
-
-        //returns JSON with data from darksky
-        $.getJSON(url + apiKey + '/' + latitude + ',' + longitude + '?callback=?', function (data) {
-            $('#temp').html(data.currently.temperature + '° F');
-            //debugger;
-            $('#minutely').html(data.minutely.summary);
-        });
+        weather(latitude, longitude);
+        suntimes(position);
+        $('#gmaps').html('<a href=\'https://maps.google.com/maps?q=' + latitude +',' + longitude + '\' target=\'_blank\'>' + "View on Google Maps");
     }
 
     function error() {
         location.innerHTML = "Unable to retrieve your location.";
     }
 
+};
+
+function weather(lat, long) {
+    var apiKey = '9add6004cd6af626a252478801c67b84';
+    var url = 'https://api.forecast.io/forecast/';
+
+    //returns JSON with data from darksky
+    $.getJSON(url + apiKey + '/' + lat + ',' + long + '?callback=?', function (data) {
+        $('#temp').html("Current temperature is: " + data.currently.temperature + '° F');
+        $('#minutely').html(data.minutely.summary);
+    });
 }
 
-weather();
+function suntimes(position) {
+    var output = document.getElementById("sun");
+    var url = 'https://api.sunrise-sunset.org/json'
+    //debugger;
+    $.getJSON(url + '?lat=' + position.coords.latitude + '&lng=' + position.coords.longitude, function (data) {
+        $('#sun').html("Sunrise: " + data.results.sunrise + ' Sunset: ' + data.results.sunset);
+    });
+};
+
+
+getLocation();
